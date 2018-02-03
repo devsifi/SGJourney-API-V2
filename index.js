436cc09ccb6035569c604ea64d32af4fd328d0a7
Siddiq <devsfiq@gmail.com>
2018-02-03 22:54:05 +0800

    Updated Package.json

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require('express')
-const PORT = process.env.PORT || 5000
+const morgan = require('morgan')
+const parser = require('body-parser')
 
+const PORT = process.env.PORT || 5000
 const app = express()
 
-app.get('/', (req, res) => res.send('Welcome to SGJourney API'))
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(morgan('dev'))
+app.use(parser.urlencoded({ extended: false }));
+app.use(parser.json());
+
+app.use('/bus', require('./routes/bus'))
+
+app.get('/', (req, res) => {
+    res.send('Welcome to SGJourney API')
+})
+
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send({
+        success: false,
+        messages: ["An error has occured while processing your request"]
+    })
+})
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
